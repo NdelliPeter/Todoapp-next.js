0f2b82d61108e98e7c96d1ba4e22c5c0cac2465a
Ndelli Peter <ndellipetex@gmail.com>
2023-01-25 02:36:40 +0100

    udate output

--- src/pages/addTask.js
+++ src/pages/addTask.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import styles from '@/styles/Home.module.css'
 import { useState, useEffect } from 'react'
-
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 
 
@@ -70,22 +70,16 @@ export default function AddTask () {
         // console.log(objectData)
     }
 
-    // function display(item) {
-    //     return [item.title, item.date, item.startTime, item.endTime].join(" ")
-    // }
 
 
 
-    const list = () => {
-        localStorage.getItem('data')
-    }
 
     return (
         <div>
             <div className='container-fluid mt-5 d-flex justify-content-center align-items-center'>
                 <form id='test' className='row justify-content-center align-items-center' name='myForm' onSubmit={submitTodo}>
                     <div className='row d-flex '>
-                        <div className='col-sm-9 col-md-9 col-lg-9'>
+                        <div className='col-sm-12 col-md-9 col-lg-9'>
                             <input 
                                 className='col-sm-12 col-md-12 col-lg-12 ' 
                                 id={styles.inputTask} 
@@ -94,32 +88,29 @@ export default function AddTask () {
                                 name="title"
                                 />
                             <input 
-                                className='col-sm-6 col-md-3 col-lg-3' 
+                                className='col-sm-4 col-md-4 col-lg-4' 
                                 id={styles.otherInput} 
                                 type={'date'} 
                                 placeholder={'Date'} 
-                                // onChange={handleDate}
                                 name="date"
                                 />
                             <input 
-                                className='col-sm-6 col-md-3 col-lg-3' 
+                                className='col-sm-4 col-md-3 col-lg-3' 
                                 id={styles.otherInput} 
                                 type={'time'} 
                                 placeholder={'Start Time'} 
-                                // onChange={handleStartTime}
                                 name="startTime"
                                 />
                             <input 
-                                className='col-sm-6 col-md-3 col-lg-3' 
+                                className='col-sm-4 col-md-3 col-lg-3' 
                                 id={styles.otherInput} 
                                 type={'time'} 
                                 placeholder={'End Time'} 
-                                // onChange={handleEndTime}
                                 name="endTime"
                                 />
                         </div>
 
-                        <div className='col-sm-7 col-md-3 col-lg-3'>
+                        <div className='col-sm-8 col-md-3 col-lg-3'>
                             <button 
                                 type='summit'
                                 className='col-sm-12 col-md-12 col-lg-12' 
@@ -142,9 +133,16 @@ export default function AddTask () {
                                 <ul className={styles.todo_list}>
                                     {
                                         todo.length >=1 ? todo.map((todo, idx) =>{
-                                            return <li className={styles.listDisplay} key={idx}>
-                                                Task: {todo.title} <br/> 
-                                                Date:{todo.date} StartTime:{todo.startTime} EndTime:{todo.endTime} </li>
+                                            return (
+                                                <div className={styles.listDisplay}>
+                                                    <li  key={idx}>
+                                                    Task: {todo.title} <br/> 
+                                                    Date:{todo.date} StartTime:{todo.startTime} EndTime:{todo.endTime} </li>
+                                                    <BsThreeDotsVertical className={styles.icon} /> 
+                                                </div>
+
+                                            )
+                                                
                                         })
                                         : 'Enter Task'
                                     }
